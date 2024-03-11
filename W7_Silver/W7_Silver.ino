75642f032985526be70a9112ca56c7fbc15e4d5a
Cooper Illingworth <22CooperLM@spa.edu>
2024-03-11 21:53:50 +0000

    initialising silver arduino file

---
+++ W7_Silver/W7_Silver.ino
@@ -0,0 +1,11 @@
+//initialising file, this will be used for creating the Silver challenge, requiring all bronze elements plus 2 types of PID control
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
